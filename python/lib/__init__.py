7e29f5a4a33b5f262332b6e0109b348cd8683780
Forest Gregg <fgregg@uchicago.edu>
2013-07-15 14:29:30 -0500

    right module names:

--- python/lib/__init__.py
+++ python/lib/__init__.py
@@ -1,2 +1,2 @@
 from factor_graph import PBinaryVariable, PFactorGraph, PMultiVariable
-from simple_inference import simple_grid, general_graph, 
+from simple_inference import simple_grid, general_graph
